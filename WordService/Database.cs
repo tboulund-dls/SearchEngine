8d073862eeaf93a9c8ac8b0e907b4f4111def38a
Thomas Boulund Meng Hansen <thomasboulundmenghansen@Thomass-MacBook-Pro-5.local>
2023-12-05 10:41:20 +0100

    Coordinating databases

--- WordService/Database.cs
+++ WordService/Database.cs
@@ -1,16 +1,12 @@
+using System.Data;
 using Microsoft.Data.SqlClient;
 
 namespace WordService;
 
 public class Database
 {
+    private Coordinator coordinator = new Coordinator();
     private static Database instance = new Database();
-    private SqlConnection _connection;
-    private Database()
-    {
-        _connection = new("Server=word-db;User Id=sa;Password=SuperSecret7!;Encrypt=false;");
-        _connection.Open();
-    }
 
     public static Database GetInstance()
     {
@@ -19,20 +15,24 @@ public class Database
 
     public void DeleteDatabase()
     {
-        Execute("DROP TABLE IF EXISTS Occurrences");
-        Execute("DROP TABLE IF EXISTS Words");
-        Execute("DROP TABLE IF EXISTS Documents");
+        foreach (var connection in coordinator.GetAllConnections())
+        {
+            Execute(connection, "DROP TABLE IF EXISTS Occurrences");
+            Execute(connection, "DROP TABLE IF EXISTS Words");
+            Execute(connection, "DROP TABLE IF EXISTS Documents");
+        }
     }
 
     public void RecreateDatabase()
     {
-        Execute("CREATE TABLE Documents(id INTEGER PRIMARY KEY, url VARCHAR(500))");
-        Execute("CREATE TABLE Words(id INTEGER PRIMARY KEY, name VARCHAR(500))");
-        Execute("CREATE TABLE Occurrences(wordId INTEGER, docId INTEGER, "
-                + "FOREIGN KEY (wordId) REFERENCES Words(id), "
-                + "FOREIGN KEY (docId) REFERENCES Documents(id))");
+        Execute(coordinator.GetDocumentConnection(), "CREATE TABLE Documents(id INTEGER PRIMARY KEY, url VARCHAR(500))");
+        Execute(coordinator.GetOccurrenceConnection(), "CREATE TABLE Occurrences(wordId INTEGER, docId INTEGER)");
+        //Execute(coordinator.GetOccurrenceConnection(), "CREATE INDEX word_index ON Occ (wordId)");
 
-        //Execute("CREATE INDEX word_index ON Occ (wordId)");
+        foreach (var connection in coordinator.GetAllWordConnections())
+        {
+            Execute(connection, "CREATE TABLE Words(id INTEGER PRIMARY KEY, name VARCHAR(500))");
+        }
     }
 
     // key is the id of the document, the value is number of search words in the document
@@ -43,7 +43,8 @@ public class Database
         var sql = @"SELECT docId, COUNT(wordId) AS count FROM Occurrences WHERE wordId IN " + AsString(wordIds) +
                   " GROUP BY docId ORDER BY count DESC;";
 
-        var selectCmd = _connection.CreateCommand();
+        var connection = coordinator.GetOccurrenceConnection();
+        var selectCmd = connection.CreateCommand();
         selectCmd.CommandText = sql;
 
         using (var reader = selectCmd.ExecuteReader())
@@ -56,7 +57,7 @@ public class Database
                 res.Add(docId, count);
             }
         }
-
+            
         return res;
     }
 
@@ -69,7 +70,8 @@ public class Database
     {
         List<string> res = new List<string>();
 
-        var selectCmd = _connection.CreateCommand();
+        var connection = coordinator.GetDocumentConnection();
+        var selectCmd = connection.CreateCommand();
         selectCmd.CommandText = "SELECT * FROM Documents WHERE id IN " + AsString(docIds);
 
         using (var reader = selectCmd.ExecuteReader())
@@ -86,10 +88,10 @@ public class Database
         return res;
     }
 
-    private void Execute(string sql)
+    private void Execute(IDbConnection connection, string sql)
     {
-        using var trans = _connection.BeginTransaction();
-        var cmd = _connection.CreateCommand();
+        using var trans = connection.BeginTransaction();
+        var cmd = connection.CreateCommand();
         cmd.Transaction = trans;
         cmd.CommandText = sql;
         cmd.ExecuteNonQuery();
@@ -98,38 +100,39 @@ public class Database
 
     internal void InsertAllWords(Dictionary<string, int> res)
     {
-        using (var transaction = _connection.BeginTransaction())
+        foreach (var p in res)
         {
-            var command = _connection.CreateCommand();
-            command.Transaction = transaction;
-            command.CommandText = @"INSERT INTO Words(id, name) VALUES(@id,@name)";
-
-            var paramName = command.CreateParameter();
-            paramName.ParameterName = "name";
-            command.Parameters.Add(paramName);
-
-            var paramId = command.CreateParameter();
-            paramId.ParameterName = "id";
-            command.Parameters.Add(paramId);
-
-            // Insert all entries in the res
-
-            foreach (var p in res)
+            var connection = coordinator.GetWordConnection(p.Key);
+            using (var transaction = connection.BeginTransaction())
             {
+                var command = connection.CreateCommand();
+                command.Transaction = transaction;
+                command.CommandText = @"INSERT INTO Words(id, name) VALUES(@id,@name)";
+
+                var paramName = command.CreateParameter();
+                paramName.ParameterName = "name";
+                command.Parameters.Add(paramName);
+
+                var paramId = command.CreateParameter();
+                paramId.ParameterName = "id";
+                command.Parameters.Add(paramId);
+
                 paramName.Value = p.Key;
                 paramId.Value = p.Value;
                 command.ExecuteNonQuery();
+                
+                transaction.Commit();
             }
 
-            transaction.Commit();
         }
     }
 
     internal void InsertAllOcc(int docId, ISet<int> wordIds)
     {
-        using (var transaction = _connection.BeginTransaction())
+        var connection = coordinator.GetOccurrenceConnection();
+        using (var transaction = connection.BeginTransaction())
         {
-            var command = _connection.CreateCommand();
+            var command = connection.CreateCommand();
             command.Transaction = transaction;
             command.CommandText = @"INSERT INTO Occurrences(wordId, docId) VALUES(@wordId,@docId)";
 
@@ -144,20 +147,21 @@ public class Database
 
             command.Parameters.Add(paramDocId);
 
-            foreach (var p in wordIds)
+            foreach (var wordId in wordIds)
             {
-                paramwordId.Value = p;
+                paramwordId.Value = wordId;
                 command.ExecuteNonQuery();
-            }
 
-            transaction.Commit();
+                transaction.Commit();
+            }
         }
     }
 
     public void InsertDocument(int id, string url)
     {
+        var connection = (SqlConnection)coordinator.GetDocumentConnection();
         var insertCmd = new SqlCommand("INSERT INTO Documents(id, url) VALUES(@id,@url)");
-        insertCmd.Connection = _connection;
+        insertCmd.Connection = connection;
 
         var pName = new SqlParameter("url", url);
         insertCmd.Parameters.Add(pName);
@@ -172,20 +176,22 @@ public class Database
     {
         Dictionary<string, int> res = new Dictionary<string, int>();
 
-        var selectCmd = _connection.CreateCommand();
-        selectCmd.CommandText = "SELECT * FROM Words";
-
-        using (var reader = selectCmd.ExecuteReader())
+        foreach (var connection in coordinator.GetAllWordConnections())
         {
-            while (reader.Read())
-            {
-                var id = reader.GetInt32(0);
-                var w = reader.GetString(1);
+            var selectCmd = connection.CreateCommand();
+            selectCmd.CommandText = "SELECT * FROM Words";
 
-                res.Add(w, id);
+            using (var reader = selectCmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32(0);
+                    var w = reader.GetString(1);
+
+                    res.Add(w, id);
+                }
             }
         }
-
         return res;
     }
 }
