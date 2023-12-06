6a85e3433c35e4617da76780353e49d6f7aae4ab
Thomas Boulund Meng Hansen <thomasboulundmenghansen@Thomass-MacBook-Pro-5.local>
2023-12-06 13:16:23 +0100

    Added caching

--- WordService/Coordinator.cs
+++ WordService/Coordinator.cs
@@ -5,6 +5,7 @@ namespace WordService;
 
 public class Coordinator
 {
+    private IDictionary<string, IDbConnection> ConnectionCache = new Dictionary<string, IDbConnection>();
     private const string DOCUMENT_DB = "document-db";
     private const string OCCURRENCE_DB = "occurrence-db";
     private const string SHORT_WORD_DB = "short-word-db";
@@ -55,8 +56,14 @@ public class Coordinator
 
     private IDbConnection GetConnectionByServerName(string serverName)
     {
-        var connection = new SqlConnection($"Server={serverName};User Id=sa;Password=SuperSecret7!;Encrypt=false;");
+        if (ConnectionCache.TryGetValue(serverName, out var connection))
+        {
+            return connection;
+        }
+        
+        connection = new SqlConnection($"Server={serverName};User Id=sa;Password=SuperSecret7!;Encrypt=false;");
         connection.Open();
+        ConnectionCache.Add(serverName, connection);
         return connection;
     }
 }
